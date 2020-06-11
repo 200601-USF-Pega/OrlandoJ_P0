279276449ff9952614607e2c209314bd771c0a70
jorlando989 <jorlando989@gmail.com>
2020-06-11 19:14:14 -0400

    add tests to gameservicetest -- need to be implemented

--- MarioKartFighter/test/com/revature/mariokartfighter/service/GameServiceTest.java
+++ MarioKartFighter/test/com/revature/mariokartfighter/service/GameServiceTest.java
@@ -2,16 +2,19 @@ package com.revature.mariokartfighter.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.revature.mariokartfighter.dao.file.CharacterRepoFile;
-import com.revature.mariokartfighter.dao.file.ItemRepoFile;
-import com.revature.mariokartfighter.dao.file.MatchRecordRepoFile;
-import com.revature.mariokartfighter.dao.file.PlayerRepoFile;
+import com.revature.mariokartfighter.dao.db.CharacterRepoDB;
+import com.revature.mariokartfighter.dao.db.ItemRepoDB;
+import com.revature.mariokartfighter.dao.db.MatchRecordRepoDB;
+import com.revature.mariokartfighter.dao.db.PlayerRepoDB;
 import com.revature.mariokartfighter.models.Player;
 
 public class GameServiceTest {
@@ -23,14 +26,14 @@ public class GameServiceTest {
 	@Before
 	public void setupNeededClasses() {
 		connectionService = new ConnectionService();
-	}
-	
-	@Before
-	public void setupDummyData() {
-		gameService = new GameService(new PlayerRepoFile(), new CharacterRepoFile(), 
-				new ItemRepoFile(), new MatchRecordRepoFile());
-		playerService = new PlayerService(new PlayerRepoFile());
-		playerID = "testingplayer";
+		gameService = new GameService(
+				new PlayerRepoDB(connectionService),
+				new CharacterRepoDB(connectionService), 
+				new ItemRepoDB(connectionService), 
+				new MatchRecordRepoDB(connectionService));
+		playerService = new PlayerService(
+				new PlayerRepoDB(connectionService));
+		playerID = "admin002";
 	}
 	
 	@Test
@@ -39,9 +42,21 @@ public class GameServiceTest {
 	}
 	
 	@Test
+	public void setCharacterShouldReturnFalseIfCharacterIsNotUnlocked() {
+		assertFalse(gameService.setCharacter("db001", "admin003"));
+	}
+	
+	@Test
 	public void setCharacterShouldChangePlayerInfoInRepo() {
-		gameService.setCharacter("Donkey Kong", playerID);
 		List<Player> retrievedPlayers = playerService.repo.getAllPlayers();
+		for (Player p : retrievedPlayers) {
+			if(p.getPlayerID().equals(playerID)) {
+				assertNotEquals("Donkey Kong", p.getSelectedCharacter().getCharacterName());
+				break;
+			}
+		}
+		gameService.setCharacter("Donkey Kong", playerID);
+		retrievedPlayers = playerService.repo.getAllPlayers();
 		for (Player p : retrievedPlayers) {
 			if(p.getPlayerID().equals(playerID)) {
 				assertEquals("Donkey Kong", p.getSelectedCharacter().getCharacterName());
@@ -56,6 +71,41 @@ public class GameServiceTest {
 	}
 	
 	@Test
+	public void setItemShouldReturnFalseIfCharacterIsNotUnlocked() {
+		assertFalse(gameService.setCharacter("db001", "admin003"));
+	}
+	
+
+	@Test
+	public void setItemShouldChangePlayerInfoInRepo() {
+		List<Player> retrievedPlayers = playerService.repo.getAllPlayers();
+		for (Player p : retrievedPlayers) {
+			if(p.getPlayerID().equals(playerID)) {
+				assertNotEquals("blue shell", p.getSelectedItem().getItemName());
+				break;
+			}
+		}
+		gameService.setItem("blue shell", playerID);
+		retrievedPlayers = playerService.repo.getAllPlayers();
+		for (Player p : retrievedPlayers) {
+			if(p.getPlayerID().equals(playerID)) {
+				assertEquals("blue shell", p.getSelectedItem().getItemName());
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void chooseRandomCharacterShouldReturnAnItemAtOrBelowLevel() {
+		
+	}
+	
+	@Test
+	public void chooseRandomItemShouldReturnAnItemAtOrBelowLevel() {
+		
+	}
+	
+	@Test
 	public void testBotFight() {
 		
 	}
@@ -65,4 +115,14 @@ public class GameServiceTest {
 		
 	}
 
+	@After
+	public void cleanUp() {
+		try {
+			connectionService.getConnection().close();
+		} catch (SQLException e) {
+			System.out.println("Exception: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
 }
